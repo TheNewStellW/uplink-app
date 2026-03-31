d76552b7157b3a04e447b07a877afa3902f5318a
Stellios Williams <stellios.williams@gmail.com>
2026-03-31 22:42:04 +1000

    Initial Commit

---
+++ Transmission RemoteTests/Transmission_RemoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  Transmission_RemoteTests.swift
+//  Transmission RemoteTests
+//
+//  Created by Stellios Williams on 31/3/2026.
+//
+
+import Testing
+@testable import Transmission_Remote
+
+struct Transmission_RemoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
